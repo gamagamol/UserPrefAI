2ef7cb0d4a0a001a70d63aa09a40e276371a97fe
Gama Ariefsadya <gama.ariefsadya@sigma.co.id>
2025-04-12 13:37:17 +0700

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="">
+      <h1 class="font-bold text-red-300">This is frontend</h1>
+    </div>
+  )
+}
+
+export default App
